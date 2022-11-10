f1901ddddedcedefc8e34ee39573b3f12101b023
Daniel Rydlewski <65160086+danielrydlewski@users.noreply.github.com>
2022-11-10 11:21:50 +0100

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,6 +1,20 @@
 node default { 
+  file {
+    ensure  => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+
+node /^db/ {
+  include role::app_db
 }
